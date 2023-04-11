141eb48d500fd3aa29c21854b045ca0eb38a6089
HP-WIN10 <HP-WIN10@LAPTOP>
2023-04-11 15:37:04 +0300

    Add project files.

---
+++ ClassLibraryHatem/WCFHatem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClassLibraryHatem
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFHatem" in both code and config file together.
+    public class WCFHatem : IWCFHatem
+    {
+   
+
+        public string GetMessage(string name)
+        {
+            return "Hello" + name;
+        }
+    }
+}
---
+++ WFAHost/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+namespace WFAHost
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost host;
+        public Form1()
+        {
+            InitializeComponent();
+            host = new ServiceHost(typeof(ClassLibraryHatem.WCFHatem));
+            host.Open();
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label1.Text = "Service Started";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            host = new ServiceHost(typeof(ClassLibraryHatem.WCFHatem));
+            host.Open();
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label1.Text = "Service Started";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            host.Close();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label1.Text = "Service Stopped";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            host.Close();
+            button1.Enabled = true;
+            button2.Enabled = false;
+
+        }
+    }
+}
